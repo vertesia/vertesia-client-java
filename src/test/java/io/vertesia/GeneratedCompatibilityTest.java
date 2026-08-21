5b1a059393f7eded3c37577fdbc18ce9e991ac0e
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2026-08-21 09:40:08 +0900

    fix: update required model fixture (#27)

--- src/test/java/io/vertesia/GeneratedCompatibilityTest.java
+++ src/test/java/io/vertesia/GeneratedCompatibilityTest.java
@@ -21,6 +21,7 @@ class GeneratedCompatibilityTest {
                         "{"
                                 + "\"id\":\"type-1\","
                                 + "\"name\":\"Default\","
+                                + "\"edit_revision\":1,"
                                 + "\"updated_by\":\"user-1\","
                                 + "\"created_by\":\"user-1\","
                                 + "\"created_at\":\"2026-01-01T00:00:00.000Z\","
